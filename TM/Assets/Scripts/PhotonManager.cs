29ba5e72b0dc61b55181869e76fe644164305b70
DESKTOP\User <ra.forero11@uniandes.edu.co>
2019-08-25 19:55:32 -0500

    Test RealTime

---
+++ TM/Assets/Scripts/PhotonManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotonManager : Photon.MonoBehaviour
+{
+    [SerializeField]private GameObject player;
+    [SerializeField]private GameObject lobbyCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings("1.0");
+    }
+
+    
+    void OnJoinedLobby()
+    {
+        PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions() { MaxPlayers = 2 }, TypedLobby.Default);
+    }
+    void OnJoinedRoom()
+    {
+        PhotonNetwork.Instantiate("Astronaut",player.transform.position,Quaternion.identity,0);
+        lobbyCamera.SetActive(false);
+    }
+
+}
---
+++ TM/Assets/Scripts/PlayerNetworking.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerNetworking : MonoBehaviour
+{
+    [SerializeField] private GameObject playerCamara;
+    [SerializeField] private MonoBehaviour[] scriptsToIgnore;
+
+    PhotonView photonView;
+    // Start is called before the first frame update
+    void Start()
+    {
+        photonView = GetComponent<PhotonView>();
+        Initialize();
+    }
+
+    void Initialize()
+    {
+        if (photonView.isMine)
+        {
+
+        }
+        else
+        {
+            playerCamara.SetActive(false);
+            foreach(MonoBehaviour item in scriptsToIgnore)
+            {
+                item.enabled=false;
+            }
+        }
+    }
+
+}
