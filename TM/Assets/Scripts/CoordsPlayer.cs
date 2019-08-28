acf70ca69c3f83bb0d39f77e598e73f3ae9511e7
DESKTOP\User <ra.forero11@uniandes.edu.co>
2019-08-28 01:28:03 -0500

    Movimiento carater despues de desicion
    Escena 2 decision, Menu Pausa, Conexion escenas, timers, manejo coordenadas y decisiones, ScriptableObjects Coordenadas/decisiones.

---
+++ TM/Assets/Scripts/CoordsPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CoordsPlayer", menuName = "CoordsPlayer")]
+
+public class CoordsPlayer : ScriptableObject
+{
+    public new string name;
+
+    
+    public int x;
+    public int y;
+    public int estId;
+    public int decisionId;
+}
+
---
+++ TM/Assets/Scripts/MenuPausa.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPausa : MonoBehaviour
+{
+    public GameObject menupausa;
+
+    private bool enable;
+    public void switchViewMenu()
+    {
+        if (enable)
+        {
+            menupausa.SetActive(false);
+            enable = false;
+        }
+        else
+        {
+            menupausa.SetActive(true);
+            enable = true;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        enable = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            switchViewMenu();
+        }
+    }
+}
