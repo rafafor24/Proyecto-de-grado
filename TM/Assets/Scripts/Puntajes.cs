15072b3cbe56adb6b32f10147ef0aab10753c489
fcAlvarez10 <fc.alvarez10@uniandes.edu.co>
2019-09-05 10:14:36 -0500

    Scriptable object puntajes

---
+++ TM/Assets/Scripts/Puntajes.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Puntaje", menuName = "Puntaje")]
+public class Puntajes : ScriptableObject 
+{
+    // Ambos jugadores seleccionaron la opción correcta
+    public int ambosBien;
+
+    // El jugador principal seleccionó la correcta y el contrario la incorrecta
+    public int unoBienOtroMal;
+
+    // Ambos jugadores seleccionaron la opción incorrecta
+    public int ambosMal;
+
+    // El jugador principal seleccionó la opción incorrecta y el contrario la correcta
+    public int unoMalOtroBien;
+}
