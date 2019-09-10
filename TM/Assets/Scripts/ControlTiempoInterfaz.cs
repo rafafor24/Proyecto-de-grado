daee8ee75c3ce8d2029b34f2406c164be5bcc6a2
fcAlvarez10 <fc.alvarez10@uniandes.edu.co>
2019-09-09 21:51:42 -0500

    script de puntajes incompleto
    Falta establecer donde se van a llamar los métodos y establecer los parametros

--- TM/Assets/Scripts/ControlTiempoInterfaz.cs
+++ TM/Assets/Scripts/ControlTiempoInterfaz.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 using TMPro;
 using System;
 
-public class ControlTiempo : MonoBehaviour
+public class ControlTiempoInterfaz : MonoBehaviour
 {
 
     public Slider sliderTiempo;
---
+++ TM/Assets/Scripts/ControlTiempoPuntaje.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlTiempoPuntaje : MonoBehaviour
+{
+    public Puntajes puntajes;
+
+    public int CambiarTiempos(bool des1, bool des2)
+    {
+        int rpta = -1;
+
+        if (des1 && des2)
+        {
+            rpta = puntajes.ambosBien;
+        }else if (des1 && !des2)
+        {
+            rpta = puntajes.unoBienOtroMal;
+        }else if (!des1 && des2)
+        {
+            rpta = puntajes.unoMalOtroBien;
+        }else if (!des1 && !des2)
+        {
+            rpta = puntajes.ambosMal;
+        }
+
+        return rpta;
+    }
+}
