21e49cdb3c81504bedb52925cd31719a6b386199
DESKTOP\User <ra.forero11@uniandes.edu.co>
2019-09-26 12:17:40 -0500

    Update MoverJugador.cs

--- TM/Assets/Scripts/MoverJugador.cs
+++ TM/Assets/Scripts/MoverJugador.cs
@@ -116,19 +116,11 @@ public class MoverJugador : MonoBehaviour
         if (moving)
         {
             float step = speed * Time.deltaTime;
+            Vector3 newvector = ml.player1?new Vector3(-1, 0, 0): new Vector3(1, 0, 0);
+                
+            jugador.transform.position = Vector3.MoveTowards(jugador.transform.position, transform.position+newvector, step);            
 
-            if (ml.player1)
-            {
-                Vector3 newvector = new Vector3(-1, 0, 0);
-                jugador.transform.position = Vector3.MoveTowards(jugador.transform.position, transform.position+newvector, step);
-            }
-            else
-            {
-                Vector3 newvector = new Vector3(1, 0, 0);
-                jugador.transform.position = Vector3.MoveTowards(jugador.transform.position, transform.position+newvector, step);
-            }
-
-            if (Vector3.Distance(jugador.transform.position, transform.position) < 0.001f)
+            if (Vector3.Distance(jugador.transform.position, transform.position+newvector) < 0.001f)
             {
                 jugador.GetComponent<MoveCharPhoton>().ChangeTrigger(QUIETO);
                 moving = false;
